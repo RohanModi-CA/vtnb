a0d9818b5437ddc55bb807d3c644d1f266276735
Rohan Modi <rohan@rohanmodi.ca>
2024-08-09 10:59:33 -0400

    fixes

--- plugin/killfrac.lua
+++ plugin/killfrac.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("CursorMovedI", {
     local col_number = vim.api.nvim_win_get_cursor(0)[2]
     local current_line = vim.api.nvim_buf_get_lines(0, line_number-1, line_number, false)[1]
     local last_word = current_line:sub(1, col_number):match("%w+$")
-    if last_word == "KILLFRAC" and not killfrac_typed then
+    if string.sub(last_word, -8) == "KILLFRAC" and not killfrac_typed then
       killfrac_typed = true
       print("True")
 	  require('killfrac').killFrac(current_line, line_number)
